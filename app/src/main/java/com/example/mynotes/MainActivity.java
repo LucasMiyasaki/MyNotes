fe7e96679b8df5fd2696c90dbd2f0bd1106d365b
Lucas Miyasaki <lhmiyasaki@gmail.com>
2024-09-12 12:01:38 -0300

    feat: carregar notas

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +15,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mynotes.db.dal.NotaDAL;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+   private ListView listView;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
          v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
          return insets;
       });
+
+      listView = findViewById(R.id.listView);
+      carregarNotas();
    }
 
    @Override
@@ -39,20 +48,24 @@ public class MainActivity extends AppCompatActivity {
 
    @Override
    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-      if(item.getItemId() == R.id.itNovaAnotacao) {
-         Intent intent=new Intent(this,FormActivity.class);
+      if (item.getItemId() == R.id.itNovaAnotacao) {
+         Intent intent = new Intent(this, FormActivity.class);
          startActivity(intent);
-      }
+      } else if (item.getItemId() == R.id.itOrdenarPrioridade)
+         Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
 
-      else if(item.getItemId() == R.id.itOrdenarPrioridade)
-         Toast.makeText(getApplicationContext(),"2",Toast.LENGTH_SHORT).show();
-
-      else if(item.getItemId() == R.id.itOrdenarOrdem)
-         Toast.makeText(getApplicationContext(),"3",Toast.LENGTH_SHORT).show();
+      else if (item.getItemId() == R.id.itOrdenarOrdem)
+         Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
 
       else
          finish();
 
       return super.onOptionsItemSelected(item);
+   }
+
+   private void carregarNotas() {
+      NotaDAL notaDAL = new NotaDAL(this);
+      List notas = notaDAL.get("");
+      listView.setAdapter(new NotaAdapter(this, R.layout.item_notas, notas));
    }
 }
